cd12cd32aa4dc702b93f11bea5d83b207f8c3b1e
Stefan Dierauf <sdierauf@gmail.com>
2014-09-08 09:47:10 -0700

    list dem cookies

--- index.php
+++ index.php
@@ -18,6 +18,20 @@
         <form action="getcookie.php" method="post">
             <div><input type="submit" value="get a cookie" /></div>
         </form>
+        <pre id='data'>
+        </pre>
     </body>
+    <script>
+        function listCookies() {
+            var theCookies = document.cookie.split(';');
+            var aString = '';
+            for (var i = 1 ; i <= theCookies.length; i++) {
+                aString += i + ' ' + theCookies[i-1] + "\n";
+            }
+            return aString;
+        }
+
+        document.getElementById('data').innerHTML = listCookies();
+    </script>
 </html>
 
